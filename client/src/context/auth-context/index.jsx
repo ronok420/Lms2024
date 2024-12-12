c615a38a989408b5ecb646cb79380e73cabf9de5
ronok420 <shahariarronok41.com>
2024-12-12 20:50:42 +0600

    sign up  and  login  authenticate

---
+++ client/src/context/auth-context/index.jsx
@@ -0,0 +1,52 @@
+import { initialSignInFormData, initialSignUpFormData } from "@/config";
+import { loginService, registerService } from "@/services";
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext(null);
+export default function AuthProvider({ children }) {
+  const [signInFormData, setSignInFormData] = useState(initialSignInFormData);
+  const [signUpFormData, setSignUpFormData] = useState(initialSignUpFormData);
+
+  async function handleRegisterUser(e) {
+    e.preventDefault();
+    const data = await registerService(signUpFormData);
+    console.log(data);
+  }
+
+
+  async function handleLoginUser(event) {
+    event.preventDefault();
+    const data = await loginService(signInFormData);
+    console.log(data, "datadatadatadatadata");
+
+    // if (data.success) {
+    //   sessionStorage.setItem(
+    //     "accessToken",
+    //     JSON.stringify(data.data.accessToken)
+    //   );
+    //   setAuth({
+    //     authenticate: true,
+    //     user: data.data.user,
+    //   });
+    // } else {
+    //   setAuth({
+    //     authenticate: false,
+    //     user: null,
+    //   });
+    // }
+  }
+  return (
+    <AuthContext.Provider
+      value={{
+        signInFormData,
+        setSignInFormData,
+        signUpFormData,
+        setSignUpFormData,
+        handleRegisterUser,
+        handleLoginUser
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
---
+++ client/src/services/index.js
@@ -0,0 +1,23 @@
+import axiosInstance from "@/api/axiosInstance";
+
+
+export async function registerService(formData){
+    const {data }=await axiosInstance.post('/auth/register',{
+        ...formData,
+        role:'user'
+      })
+      return data;
+
+}
+export async function loginService(formData){
+  console.log(formData,"for  testing  login");
+  
+    const {data }=await axiosInstance.post('/auth/login',formData);
+      return data;
+
+}
+export async function checkAuth(formData){
+    const {data }=await axiosInstance.get('/auth/check-auth');
+      return data;
+
+}
